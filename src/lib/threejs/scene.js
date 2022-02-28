c911f06ee6dffdfbc0a128c56892eada5e9d2f4e
John Garcia <johnkegd@gmail.com>
2022-02-28 01:48:24 +0100

    added threejs dependency, added demo route to navbar, draco decoder added, human model for scrolling test added, srolling test demo added, scrolling working

---
+++ src/lib/threejs/scene.js
@@ -0,0 +1,93 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+
+const scrollingPositions = {
+    scrollY: 0,
+    scrollX: 0,
+};
+const windowSizes = {
+    height: 0,
+    with: 0,
+}
+window.scrolling = scrollingPositions;
+
+let renderer, controls;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(35, 1, 0.1, 1000);
+camera.position.y = 20;
+camera.position.z = 10;
+
+function addMeshes() {
+    const geometry = new THREE.BoxGeometry();
+    const boxMesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
+        color: "red",
+    }));
+
+    scene.add(boxMesh);
+}
+
+function addModel() {
+    const loader = new GLTFLoader();
+
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath('threejs/draco/');
+    loader.setDRACOLoader(dracoLoader);
+
+    loader.load('threejs/models/human.glb',
+        function (model) {
+            console.log(model);
+            scene.add(model.scene);
+        },
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + "% loaded");
+        },
+        function (error) {
+            console.log("Error loading object: ", error);
+        }
+    );
+}
+
+const axesHelper = new THREE.AxesHelper(8);
+scene.add(axesHelper);
+
+export const updateSizes = function (sizes) {
+    windowSizes.height = sizes.height;
+    windowSizes.with = sizes.width;
+    renderer.setSize(sizes.width, sizes.height, false);
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+}
+
+export const scolling = function () {
+    scrollingPositions.scrollY = window.scrollY;
+    scrollingPositions.scrollX = window.scrollX;
+}
+window.camera = camera;
+
+
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    camera.position.y = -  scrollingPositions.scrollY * 0.03 + 20;
+
+    renderer.render(scene, camera);
+}
+
+function addLights() {
+    const light = new THREE.DirectionalLight({ color: "#fafa00", intensity: 10 });
+    light.position.x = 3;
+    light.position.y = 12;
+    light.position.z = 6;
+    scene.add(light);
+}
+
+export const init = function (el, uiContainer) {
+    //addMeshes();
+    addLights();
+    addModel();
+    renderer = new THREE.WebGLRenderer({ canvas: el, antialias: true });
+    updateSizes({ width: el.width, height: el.height });
+    animate();
+}
---
+++ src/routes/demos/index.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { onMount } from 'svelte';
+	import { init, updateSizes, scolling } from '$lib/threejs/scene';
+	let width, height, el, scrollY, scrollX;
+	onMount(() => {
+		el.width = width;
+		el.height = height;
+		init(el);
+	});
+</script>
+
+<svelte:window
+	bind:innerWidth={width}
+	bind:innerHeight={height}
+	bind:scrollY
+	bind:scrollX
+	on:scroll={scolling}
+	on:resize={updateSizes({ width, height })}
+/>
+
+<canvas bind:this={el} {width} {height} />
+<section>Section 1</section>
+<section>Section 2</section>
+<section>Section 3</section>
+<section>Section 4</section>
+<section>Section 5</section>
+
+<style>
+	canvas {
+		position: fixed;
+		top: 0;
+		left: 0;
+		outline: none;
+		z-index: -1;
+	}
+
+	section {
+		color: white;
+		height: 400px;
+	}
+</style>
