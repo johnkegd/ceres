ae2e025d2704c9aa3119c51c54e9f44b96c52a4f
John Garcia <johnkegd@gmail.com>
2022-04-19 01:00:34 +0200

    [landing] - temporal deploy

---
+++ src/lib/SocialMedia.svelte
@@ -0,0 +1,42 @@
+<div class="social_media">
+	<div class="icon">
+		<a href="https://www.instagram.com/ceres.creations_" target="_Blank">
+			<i class="fab fa-instagram" />
+		</a>
+	</div>
+	<div class="icon">
+		<a href="https://www.artstation.com/ceres3" target="_Blank">
+			<i class="fab fa-deviantart" />
+		</a>
+	</div>
+	<div class="icon">
+		<a href="https://www.linkedin.com/in/manuel-duran-a99280217/" target="_Blank">
+			<i class="fab fa-linkedin" />
+		</a>
+	</div>
+	<div class="icon">
+		<a href="https://github.com/manuelmid" target="_Blank">
+			<i class="fab fa-github" />
+		</a>
+	</div>
+</div>
+
+<style>
+	/** Social media banner **/
+
+	.button .icon i {
+		font-size: 25px;
+		line-height: 60px;
+		transition: all 0.3s ease-in-out;
+	}
+
+	.social_media {
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+	}
+	.social_media i {
+		font-size: 40px;
+		margin-right: 14px;
+	}
+</style>
